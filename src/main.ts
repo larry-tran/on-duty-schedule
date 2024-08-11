a1039e0900c564e735b2af153d2d8d7ee9977d0c
LARRYTRAN\longt <longtran4949@gmail.com>
2024-08-11 12:42:37 +0700

    make logger show

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'], // Enable all log levels
+  });
   await app.listen(3000);
 }
 bootstrap();
